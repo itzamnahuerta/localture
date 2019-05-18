dec4c2ef0e32066a6f2d53ae2aebf7c20b0b3ffb
Wayne Wilson <wynwlsn86@gmail.com>
2019-05-18 14:04:59 -0400

    added state and wrote function to submit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './styles/App.scss';
 import LandingPage from './components/LandingPage/LandingPage';
 import { login } from './services/authApi';
+import { addUser } from './services/usersApi';
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~
 // import LoginForm from './components/LoginForm/LoginForm.jsx';
@@ -14,13 +15,19 @@ class App extends Component {
       authenticated: false,
       error:false,
       username: '',
-      password: ''
+      password: '',
+      createdUser: false,
+      city: '',
+      country: '',
+      email: '',
+      first_name: '',
+      last_name: ''
+
     }
     this.handleLogin = this.handleLogin.bind(this);
   }
 
   handleLoginFormChange = (e) => {
-
     const fieldName = e.target.name;
     const value = e.target.value;
     this.setState({
@@ -43,6 +50,28 @@ class App extends Component {
         console.log(e.message);
     }
   }
+
+  handleSignUp = async(e) => {
+    e.preventDefault();
+    if(!this.state.username || !this.state.password || !this.state.home_area || !this.state.first_name || !this.state.last_name || !this.state.email){
+        alert('Please fill in all fields')
+    }
+    else {
+        let newUser = {
+            username: this.state.username,
+            password: this.state.password,
+            home_area: `${this.state.city}, ${this.state.country}`,
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
+            email: this.state.email
+
+        }
+        await addUser(newUser);
+        console.log('user created')
+        this.setState({createdUser: true});
+        this.setState({authenticated: true});
+    }
+}
 
 
   render() {
