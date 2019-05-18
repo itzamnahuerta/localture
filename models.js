889b1b2b776096c7422e30811196800b38437dd9
Wayne Wilson <wynwlsn86@gmail.com>
2019-05-17 20:43:55 -0400

    init push

--- models.js
+++ models.js
@@ -28,127 +28,35 @@ const City = localtureDb.define('city', {
     },
   })
 
-   const  = pawDb.define('adopters', {
+   const  Message = pawDb.define('adopters', {
     first: {
         type: Sequelize.STRING,
         allowNull: false
     },
-    last: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    dob:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    license: {
-        type: Sequelize.STRING,
-        allowNull: true
-    },
-    rent_own:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    num_pets:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    num_children:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    allergies:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    vet:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    phone: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false
-    } ,
-    address: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    city:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    state: {
-        type: Sequelize.STRING,
-        allownull: false
-    },
-    zip: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    house_size: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    animal_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    }
+    
 })
 
-const Volunteer = pawDb.define('volunteers', {
-    name: {
-        type:Sequelize.STRING,
-        allowNull: false
-       },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false
-       },
-    phone: {
-        type: Sequelize.STRING,
-        allowNull: false
-       },
-    num_animals: {
-        type: Sequelize.STRING,
-        allowNull: true
-       },
-    preference: {
-        type:Sequelize.STRING,
-        allowNull: true
-    },
-    additional_info: {
-        type: Sequelize.STRING,
-        allowNull: true
-    }
-   })
-
-
-Pet.belongsToMany(Volunteer, {
-    through: 'pets_to_vol_xref',
-    foreignKey: 'animal_id'});
-Volunteer.belongsTo(Pet);
 
 
 
-Adopter.belongsToMany(Pet, {
-    through: 'adopt_to_pets_xref',
-    foreignKey: 'adopt_id'});
+// Pet.belongsToMany(Volunteer, {
+//     through: 'pets_to_vol_xref',
+//     foreignKey: 'animal_id'});
+// Volunteer.belongsTo(Pet);
 
 
-AuthUser.beforeCreate(async (user,options) => {
-    const hashedPassword = await bcrypt.hash(user.password, 12);
-    user.password = hashedPassword;
-  })
+
+// Adopter.belongsToMany(Pet, {
+//     through: 'adopt_to_pets_xref',
+//     foreignKey: 'adopt_id'});
+
+
+// AuthUser.beforeCreate(async (user,options) => {
+//     const hashedPassword = await bcrypt.hash(user.password, 12);
+//     user.password = hashedPassword;
+//   })
 
 
   module.exports = {
-    pawDb,
-    AuthUser,
-    Pet,
-    Adopter,
-    Volunteer
+ 
   }
