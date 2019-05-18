71f019a1e30e775d06d40e4a0795dc3cd87ec43f
Itzamna <huerta.ek@gmail.com>
2019-05-18 11:02:19 -0400

    log in form drafted in landing-page module

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import './styles/App.scss';
+import LandingPage from './components/LandingPage/LandingPage';
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~
+// import LoginForm from './components/LoginForm/LoginForm.jsx';
 
 function App() {
   return (
     <div className="App">
-        <h1> localture </h1>
+        <h1> localture* </h1>
+        <LandingPage/>
     </div>
   );
 }
---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import LoginForm from '../LoginForm/LoginForm'
+import SignupForm from '../SignupForm/SignupForm';
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1> landing page </h1>
+        <LoginForm/>
+        {/* <SignupForm />  */}
+      </div>
+    )
+  }
+}
