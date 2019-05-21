0e8374eff2d46fc8db41920786c6a7727eb86dbb
Wayne Wilson <wynwlsn86@gmail.com>
2019-05-21 13:40:42 -0400

    added assets for profile pics and icons. further profile build out

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import LoginForm from '../LoginForm/LoginForm'
-import LandingPage from '../LandingPage/LandingPage'
+import ProfileInterests from '../ProfileInterests/ProfileInterests'
 import {Redirect, Link} from 'react-router-dom';
+import Active from '../../assets/active.png';
+import Entertainment from '../../assets/entertainment.png';
+import Food from '../../assets/food.png';
+import Music from '../../assets/music.png';
 
 class UserProfile extends Component {
     render() {
@@ -14,9 +17,40 @@ class UserProfile extends Component {
         <Link to='/othersprofile'>SOMEONE ELSEEEEEE</Link>
         <Link to='/travelers_near_you'>Travelers Near You</Link>
         <Link to='/interests'>Interests</Link>
-                    <div>ALL THIS SSTUFF</div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <h1>My Profile</h1>
+                    </div>
+
+                    <div>
+                        <div>IMAGE</div>
+                        <div>HOME AREA</div>
+                    </div>
+
+                    <div>
+                        <h1>Interests</h1>
+                    </div>
+                    <div>Music</div>
+
+                    
+                    <div>
+                        {/* .map through instrests and reder one for each */}
+                        <ProfileInterests 
+                            name='Music'
+                            image={<img src={Music} alt='' />}
+                        />
+                        <ProfileInterests 
+                            name='Food'
+                            image={<img src={Food} alt='' />}
+                        />
+                        <ProfileInterests 
+                            name='Entertainment'
+                            image={<img src={Entertainment} alt='' />}
+                        />
+                        <ProfileInterests 
+                            name='Active'
+                            image={<img src={Active} alt='' />}
+                        />
+                    </div>
                 </div>
             )
         }
