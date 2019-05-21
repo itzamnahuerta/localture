31e640a5504e855bc5ac286e13c07a7e78a88929
Wayne Wilson <wynwlsn86@gmail.com>
2019-05-21 16:18:31 -0400

    interets and user profile 99 percent built. awaiting style

--- client/src/components/ProfileInterests/ProfileInterests.jsx
+++ client/src/components/ProfileInterests/ProfileInterests.jsx
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';import active from '../../assets/active.png';
+import Interests from '../Interests/Interests'
 import Entertainment from '../../assets/entertainment.png';
 import Food from '../../assets/food.png';
 import Music from '../../assets/music.png';
 import Active from '../../assets/active.png';
+import Note from '../../assets/note.png'
 
 class ProfileInterests extends Component {
+    constructor(){
+        super();
+        this.state = {
+            editInterests: false
+        }
+    }
+
+    handleEditInterests = (e) => {
+        e.preventDefault();
+        this.setState({editInterests: true})
+    }
+
     render() {
-        return (
-            <div>
-                {this.props.image}
-                <div>{this.props.name}</div>
-                <ul>
-                    {/* .map through sub */}
-                    <li>Sub Category</li>
-                    <li>Sub Category</li>
-                    <li>Sub Category</li>
-                    
-                </ul>
-            </div>
-        );
+        if(this.state.editInterests){
+            return <h1>working</h1>
+        
+        }
+        else if(!this.state.editInterests) {
+            return (
+                <div>
+                    {this.props.image}
+                    <img 
+                    src={Note} 
+                    onClick={this.props.handleEditInterests}
+                    id={this.props.name}/>
+                    <div>{this.props.name}</div>
+                    <ul>
+                        <li>Sub Category</li>
+                        <li>Sub Category</li>
+                        <li>Sub Category</li>
+                        <li>Sub Category</li>
+                    </ul>
+                </div>
+            );
+        }
     }
 }
 
