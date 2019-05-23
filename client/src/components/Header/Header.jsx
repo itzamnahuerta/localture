97433723125555e709678143a29d6553b8af761f
Itzamna <huerta.ek@gmail.com>
2019-05-23 11:13:22 -0400

    trying to resolve merge

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header">
-      <h4>LOCALTURE </h4>
+      <div className="page-title">LOCALTURE </div>
       </div>
     )
   }
